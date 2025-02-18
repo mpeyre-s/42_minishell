70847da3373198669593dfe53b92d9de10f388a9
mathispeyre <90681104+mpeyre-s@users.noreply.github.com>
2025-02-18 15:00:17 +0100

    minishell inception

--- execution/utils/bin_utils.c
+++ execution/utils/bin_utils.c
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 00:27:39 by mathispeyre       #+#    #+#             */
-/*   Updated: 2025/02/17 22:12:52 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2025/02/18 14:43:31 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 /** Executes binary instructions with error recovery capabilities */
 int	handle_child_process(t_command *cmd, char ***env, char *path)
 {
+	char	*self_bin_path;
+
+	self_bin_path = ft_strjoin("/", cmd->args[0]);
+	if (!ft_strncmp(self_bin_path, path, ft_strlen(self_bin_path)))
+	{
+		free(path);
+		path = ft_strdup(cmd->args[0]);
+	}
+	free(self_bin_path);
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 	if (execve(path, cmd->args, *env) == -1)
@@ -29,7 +38,27 @@ int	handle_child_process(t_command *cmd, char ***env, char *path)
 /** Split the PATH environment variable into an array of paths */
 char	**split_path(char *path_env)
 {
+	char	**paths;
+	int		i;
+	int		len;
+	char	**new_paths;
+
+	len = 0;
+	i = -1;
 	if (!path_env)
 		return (NULL);
-	return (ft_split(path_env, ':'));
+	paths = ft_split(path_env, ':');
+	if (!paths)
+		return (NULL);
+	while (paths[len])
+		len++;
+	new_paths = malloc((len + 2) * sizeof(char *));
+	if (!new_paths)
+		return (NULL);
+	new_paths[0] = ft_strdup("");
+	while (++i < len)
+		new_paths[i + 1] = paths[i];
+	new_paths[len + 1] = NULL;
+	free(paths);
+	return (new_paths);
 }
